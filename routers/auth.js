50a7802dcd2fe3c38e46c7f3d375df581f6d64bc
Shubham-Satpathy <satpathyshubham.0302@gmail.com>
2024-12-08 10:37:58 +0530

    new hash

--- routers/auth.js
+++ routers/auth.js
@@ -2,13 +2,15 @@ const express = require('express');
 const app=express();
 const router = express.Router();
 const User = require('../models/User');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 // User registration
 router.post('/register', async (req, res) => {
     try {
         const { username, password } = req.body;
-        const hashedPassword = await bcrypt.hash(password, 10);
+        const salt = bcrypt.genSaltSync(10);
+        const hashedPassword = bcrypt.hashSync(password, salt);
+        // const hashedPassword = await bcrypt.hash(password, 10);
         const user = new User({ username, password: hashedPassword });
         await user.save();
         res.redirect("/");
@@ -26,7 +28,8 @@ router.post('/login', async (req, res) => {
         if (!user) {
             return res.render("./auth/login",{message:"Invalid Username or Password"});
         }
-        const passwordMatch = await bcrypt.compare(password, user.password);
+        const passwordMatch = bcrypt.compareSync(password, user.password);
+        // const passwordMatch = await bcrypt.compare(password, user.password);
         if (!passwordMatch) {
             return res.render("./auth/login",{message:"Invalid Username or Password"});
         }
