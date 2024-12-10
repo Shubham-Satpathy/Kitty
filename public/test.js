5396ac3fb7a871e72186146eb460be39f4dbe26c
Shubham-Satpathy <141399400+Shubham-Satpathy@users.noreply.github.com>
2024-12-10 10:49:05 +0530

    add Date

--- public/test.js
+++ public/test.js
@@ -1,7 +1,7 @@
 const month=["","January","February","March","April","May","June","July","August","September","October","November","December"];
 const date=document.getElementById("date");
 const d=date.textContent.split("/");
-date.textContent=`${d[1]}-${month[d[2]]}-${d[0]}`
+date.textContent=`${d[2]}-${month[d[1]]}-${d[0]}`
 const p=document.querySelector('#entry');
 p.innerHTML=p.textContent;
 console.log(p);
